ff3533649b560cf5e50dd490653710a913600294
Julianeme <juliantve@gmail.com>
2021-10-02 17:51:52 -0500

    square | area_of_my_square corrected: width * heigh

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
+"""
+Definition of square class and its methods
+"""
+
 
 class square():
-    
+    """
+    definition of square class
+    """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """constructor"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Magic method str"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
