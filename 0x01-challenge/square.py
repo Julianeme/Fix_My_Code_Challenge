63250ac9ab3b5b54bf894def1ab5e936505179dd
Julianeme <juliantve@gmail.com>
2021-10-02 18:26:47 -0500

    square | function change dto use only on side parameter

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -9,32 +9,27 @@ class square():
     definition of square class
     """
     width = 0
-    height = 0
 
     def __init__(self, *args, **kwargs):
         """constructor"""
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        if args is not None:
-            self.width = args[1]
-            self.height = args[2]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
         """ Perimeter of the square """
-        return (self.width * 2) + (self.height * 2)
+        return (self.width * 4)
 
     def __str__(self):
         """Magic method str"""
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.width)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
