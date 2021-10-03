0dde7103657f701203f3d0c303facc3cf1a8c860
Julianeme <juliantve@gmail.com>
2021-10-02 20:22:09 -0500

    Square | attept to fix class definition

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
-"""
-Definition of square class and its methods
-"""
 
 
-class Square():
-    """
-    definition of square class
-    """
+class square():
+
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """Constructor"""
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        else:
-            self.width = 0
-            self.height = 0
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return (self.width * self.height)
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """ Perimeter of the square """
-        return ((self.width * 2) + (self.height * 2))
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Magic method str"""
-        return ("{}/{}".format(self.width, self.height))
+        return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
-        s = Square(width=12, height=9)
-        print(s)
-        print(s.area_of_my_square())
-        print(s.PermiterOfMySquare())
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
